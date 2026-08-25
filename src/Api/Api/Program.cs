011656726cd8c17bd377132eef0b6aed1e185296
Jorge <jshernandez993@gmail.com>
2026-08-24 18:53:47 -0600

    Add paquete de MediatR y configuraciones

--- src/Api/Api/Program.cs
+++ src/Api/Api/Program.cs
@@ -1,9 +1,11 @@
+using Core;
 using Persistence.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
+builder.Services.AddApplication();
 builder.Services.AddPersistence(builder.Configuration);
 
 var app = builder.Build();
