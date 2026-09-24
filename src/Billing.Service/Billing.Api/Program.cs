447195b0da47d85d0918168487b5a5b34cfb2002
Pedro Garcia <GL202402@upes.edu.sv>
2026-09-23 19:10:02 -0600

    feat: agregar Scalar UI para probar la API

--- src/Billing.Service/Billing.Api/Program.cs
+++ src/Billing.Service/Billing.Api/Program.cs
@@ -1,5 +1,6 @@
 using Billing.Core;
 using Billing.Persistence.Extensions;
+using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,6 +14,7 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.MapScalarApiReference();
 }
 
 app.UseHttpsRedirection();
